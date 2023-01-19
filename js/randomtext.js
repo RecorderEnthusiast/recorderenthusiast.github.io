df9ac24fee0fa02bb56411237ee2d2eaf4b37270
RecorderEnthusiast <85328186+RecorderEnthusiast@users.noreply.github.com>
2023-01-19 11:02:22 -0600

    Add files via upload

--- js/randomtext.js
+++ js/randomtext.js
@@ -1,4 +1,4 @@
 (function replaceTitle() {
-  var verbs = ['kill', 'love', 'smile at', 'annihilate', 'drink cold beers with on a saturday night', 'fight in the ring', 'DESTROY. BUILD. DESTROY!', 'high-five', 'find the truth about', 'express how funny the loss meme is, you know, | || || |_, to',];
+  var verbs = ['love', 'smile at', 'annihilate', 'drink cold beers with on a saturday night', 'exterminate', 'squash like a bug', 'blackmail', 'smash over the head with a comically large frying pan', 'fight in the ring', 'DESTROY. BUILD. DESTROY!', 'high-five', 'find the truth about', 'express how funny the loss meme is, you know, | || || |_, to',];
   document.getElementById('listTitle').innerHTML = "List of people I want to " + verbs[Math.floor(Math.random() * verbs.length)];
 })();
